43b814007de87f450e38994fa20d8b1d5b55e274
Doctair <centit@outlook.com>
2026-07-24 10:30:41 -0400

    Update Hyperset.bat to take the focus on launch.

--- Staging/hyperset.bat
+++ Staging/hyperset.bat
@@ -2,5 +2,7 @@
 title HyperPilot Test Terminal
 set PATH=%PATH%;C:\resources
 cd /d C:\resources
-powershell.exe -ExecutionPolicy Bypass -WindowStyle Normal -NoProfile -NoExit -command "cls"
+rem powershell.exe -ExecutionPolicy Bypass -WindowStyle Normal -NoProfile -NoExit -command "cls"
+powershell.exe -ExecutionPolicy Bypass -WindowStyle Normal -NoProfile -NoExit -command ^
+"Add-Type -Name Win -Namespace Console -MemberDefinition '[DllImport(\"kernel32.dll\")] public static extern IntPtr GetConsoleWindow(); [DllImport(\"user32.dll\")] public static extern bool SetForegroundWindow(IntPtr hWnd);'; [Console.Win]::SetForegroundWindow([Console.Win]::GetConsoleWindow()); cls"
 exit /b
